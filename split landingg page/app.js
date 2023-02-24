0b10d05c97df251c09c2f472649b9316bcfce284
adeeyogee <adeeyogee@gmail.com>
2023-02-24 23:15:21 +0200

    split page

--- split landingg page/app.js
+++ split landingg page/app.js
@@ -1,7 +1,9 @@
+//*Получаем блоки в переменные
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
+//* При наведении мыши, добавляются и удаляются соответствующие классы
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
